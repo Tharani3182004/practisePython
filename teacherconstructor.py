91d6776d8dddd0ad2ef7ba649717f5aa75ae5ff1
Tharani <tharani1916@gmail.com>
2026-01-30 08:08:56 +0530

    add teacherconstructor.py

---
+++ teacherconstructor.py
@@ -0,0 +1,21 @@
+class Teacher:
+    def __init__(self):
+        self.name=""
+        self.regno=""
+    def display(self):
+        print(self.name)
+        print(self.regno)
+
+t1=Teacher()
+
+t1.name="thara"
+t1.regno="53"
+
+t2=Teacher()
+
+t2.name="nandini"
+t2.regno="107"
+
+t1.display()
+t2.display()
+
