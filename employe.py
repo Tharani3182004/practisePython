928aab5c106415877eafb8837871b1db3f8b9b2b
Tharani <tharani1916@gmail.com>
2026-02-11 10:32:28 +0530

    add employe.py

---
+++ employe.py
@@ -0,0 +1,14 @@
+class employee():
+    def __init__(self,name,salary):
+        self.name=name
+        self.salary=salary
+class manager(employee):
+    def __init__(self,name,salary,department):
+        super().__init__(name,salary)
+        self.department=department
+    
+    def display(self):
+        print(self.name,self.salary,self.department)
+
+m1=manager("thara","100000","ece")
+m1.display()
